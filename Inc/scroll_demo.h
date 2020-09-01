283e63b6e0b934ac4bbf63a46cfdb4b27d9bd1e6
vladkk345 <55635774+vladkk345@users.noreply.github.com>
2020-09-01 19:04:02 +0300

    Delete scroll_demo.h

--- Inc/scroll_demo.h
+++
@@ -1,9 +0,0 @@
-#ifndef _SCROLL_DEMO_H
-#define _SCROLL_DEMO_H
-
-#include "stm32f0xx.h"
-
-
-void demo_ScrollBuff(uint8_t *pBuff, uint16_t ScrollLines, uint8_t RightToLeft);
-
-#endif /* __SCROLL_DEMO_H__ */
